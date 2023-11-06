225760c471d530388bac29ca39293495daf0b529
ChanheeKim <cgim971@gmail.com>
2023-11-06 09:32:20 +0900

    Unity

--- Assets/02.Scripts/Server/UDPClient.cs
+++ Assets/02.Scripts/Server/UDPClient.cs
@@ -7,29 +7,29 @@ using UnityEngine;
 
 public class UDPClient : MonoBehaviour
 {
-    private readonly string IP = "127.0.0.1";
-    private readonly int PORT = 5000;
+    //private readonly string IP = "127.0.0.1";
+    //private readonly int PORT = 5000;
 
-    private UdpClient _udpClient;
-    private IPEndPoint _serverEndPoint;
+    //private UdpClient _udpClient;
+    //private IPEndPoint _serverEndPoint;
 
-    private void Start()
-    {
-        _udpClient = new UdpClient();
-        _serverEndPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
-    }
+    //private void Start()
+    //{
+    //    _udpClient = new UdpClient();
+    //    _serverEndPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
+    //}
 
-    public void SendData(string message)
-    {
-        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-        _udpClient.Send(messageBytes, messageBytes.Length, _serverEndPoint);
-    }
+    //public void SendData(string message)
+    //{
+    //    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+    //    _udpClient.Send(messageBytes, messageBytes.Length, _serverEndPoint);
+    //}
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SendData("Hello, Server");
-        }
-    }
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        SendData("Hello, Server");
+    //    }
+    //}
 }
